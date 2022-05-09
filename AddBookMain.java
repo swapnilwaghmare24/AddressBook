eb6329b7e36a94f00351c284717108bcd85076af
swapnilwaghmare24 <iamswapnilengg@gmail.com>
2022-05-09 18:23:59 +0530

    contacts added to AddressBook

--- AddBookMain.java
+++ AddBookMain.java
@@ -3,5 +3,12 @@ package AddressBookUpdated;
 public class AddBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Program");
+        AddressBook ad = new AddressBook();
+        ad.getContact();
+        for (Contacts c:ad.lc) {
+            System.out.println(c);
+
+
+        }
     }
 }
