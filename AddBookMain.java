80190bb811c00f6ad0253990963ac85108cb2528
swapnilwaghmare24 <iamswapnilengg@gmail.com>
2022-05-09 18:48:37 +0530

    contact edit

--- AddBookMain.java
+++ AddBookMain.java
@@ -5,6 +5,7 @@ public class AddBookMain {
         System.out.println("Welcome to AddressBook Program");
         AddressBook ad = new AddressBook();
         ad.getContact();
+        ad.editContact();
         for (Contacts c:ad.lc) {
             System.out.println(c);
 
